807b7350951b72423afb3b7a1eadfc261d5b4253
Andrés Abud <106325634+andyabud@users.noreply.github.com>
2022-06-14 18:09:16 -0400

    Update evaluacion_estadistica.R

--- evaluacion_estadistica.R
+++ evaluacion_estadistica.R
@@ -38,10 +38,10 @@ data_1 <- data_1 %>%
 options(scipen = 999)
 
 
-# Pregunta 2 --------------------------------------------------------------
+# Pregunta 2 -------------------------------------------------------------------
 
 
-## 2.a ---------------------------------------------------------------------
+## 2.a - Probabilidad que llueva hoy--------------------------------------------
 
 # Para ver la probabilidad hacer una tabla para ver la cantidad de días que sí
 # llueve y luego un prop.table
@@ -51,7 +51,7 @@ table(data_1$lluvia_hoy) %>%
 ## La probabilidad que llueva hoy es de 22,88%
 
 
-## 2.b ---------------------------------------------------------------------
+## 2.b - Probabilidad que un día tenga menos de 8 horas de sol------------------
 
 # Ver cuántos días con menos de 8 horas de sol hay en la tabla, para ello
 # hace un nuevo dataframe en donde se cuente la cantidad de días que hay <8
@@ -65,7 +65,7 @@ prop.table(data_1_b)*100
 ## Hay un 43,81% de probabilidad que un día tenga menos de 8 horas de sol
 
 
-## 2.c ---------------------------------------------------------------------
+## 2.c - Probabilidad de que la temperatura a las 3pm sea sobre 20°C en invierno----
 
 # Hacer un segundo dataframe con filtro en invierno
 data_1_c <- data_1 %>% 
@@ -77,7 +77,7 @@ prop.table(data_1_c)*100
 ## La probabilidad que la temperatura sea <20º en invierno es de 24,78%
 
 
-## 2.d ---------------------------------------------------------------------
+## 2.d - Probabilidad de que lluevan entre 5mm y 10mm, en un clima Koppen Subtropical----
 
 data_1_d <- data_1 %>% 
   filter(koppen == "Subtropical")
@@ -86,13 +86,13 @@ data_1_d %>%
   count(lluvia >= 5 & lluvia <= 10) %>%
   prop.table()*100
 
-## La posibilidad que llueva entre 5 a 10mm en un clima Koppen Subtropical es de
+## La probabilidad que llueva entre 5 a 10mm en un clima Koppen Subtropical es de
 # 5,37%
 
 
-## 2.e ---------------------------------------------------------------------
+## 2.e - Repetir con data original ---------------------------------------------
 
-### a - Los resultados varían casi en un 1%-------------------------------------
+### a - Probabilidad que llueva hoy--------------------------------------------
 
 ## Con la muestra aleatoria
 table(data_1$lluvia_hoy) %>% 
@@ -102,8 +102,10 @@ table(data_1$lluvia_hoy) %>%
 table(data$LluviaHoy) %>% 
   prop.table()*100 #21.96%
 
+# Los resultados varían casi en un 1%
 
-### b - Los resultados varían casi en 0.5%--------------------------------------
+
+### b - Probabilidad que un día tenga menos de 8 horas de sol---------------------
 
 ## Con la muestra aleatoria
 data_1_b <- data_1 %>%
@@ -116,7 +118,9 @@ data %>%
   count(Sol <8) %>% 
   prop.table()*100 #44,21%
 
-### c - Los resultados varían más de 1%-----------------------------------------
+# Los resultados varían casi en 0.5%
+
+### c - Probabilidad de que la temperatura a las 3pm sea sobre 20°C en invierno----
 
 ## Con la muestra aleatoria
 data_1_c <- data_1 %>% 
@@ -131,7 +135,9 @@ data %>%
   count(Temp3pm >20) %>% 
   prop.table()*100 # 25,92%
 
-### d - Los resultados varían aproximadamente 0,2%------------------------------
+# Los resultados varían más de 1%
+
+### d - Probabilidad de que lluevan entre 5mm y 10mm, en un clima Koppen Subtropical----
 
 ## Con la muestra aleatoria
 data_1_d <- data_1 %>% 
@@ -147,8 +153,9 @@ data %>%
   count(Lluvia >= 5 & Lluvia <= 10) %>% 
   prop.table()*100 # 5,18%
 
+# Los resultados varían aproximadamente 0,2%
 
-## Conclusión --------------------------------------------------------------
+## Conclusión ------------------------------------------------------------------
 
 # La muestra obtenida sí es representativa ya que el % de variación es de máximo
 # 1.4%, sin embargo podría ser más representativa aún, y para ello considero que
@@ -156,13 +163,18 @@ data %>%
 # el dataset completo y las muestras obtenidas
 
 
-# Pregunta 3 --------------------------------------------------------------
+# Pregunta 3 -------------------------------------------------------------------
+
+
+## Generar muestreo estratificado ----------------------------------------------
 
 
 # Se establece la semilla
 set.seed(2022)
+
 # Hacer una tabla de proporciones de la variable Koppen
 prop.table(table(data$Koppen))
+
 nstrata(n = 4997, wh = c(0.02985, 0.08825, 0.35635, 0.385875, 0.13967)) 
 # Se hizo con un tamaño de muestra menor ya que al ocupar n=5000 entregaba una
 # muestra de 5003 sujetos
@@ -190,12 +202,14 @@ estrat_koppen <- data %>%
 # Verificar si está correctamente estratificada
 table(estrat_koppen$Koppen) %>% 
   prop.table()
+
 table(data$Koppen) %>% 
   prop.table()
+
 # Los valores tienen una leve variación a partir de las milésimas
 
 
-## 3.a ---------------------------------------------------------------------
+## 3.a - Probabilidad de que llueva hoy-----------------------------------------
 
 ### Con el dataset original
 
@@ -211,7 +225,7 @@ table(estrat_koppen$LluviaHoy) %>%
 
 # La probabilidad que llueva hoy es de 22,36%
 
-## 3.b ---------------------------------------------------------------------
+## 3.b - Probabilidad de que un día tenga menos de 8 horas de sol----------------
 
 ### Con el dataset original
 
@@ -229,7 +243,7 @@ estrat_sol <- estrat_koppen %>%
 prop.table(estrat_sol)*100
 # Hay un 44.09% de probabilidad que un día tenga menos de 8 horas de sol
 
-## 3.c ---------------------------------------------------------------------
+## 3.c - Probabilidad de que la temperatura a las 3pm sea sobre 20°C en invierno----
 
 ### Con el dataset original
 
@@ -249,7 +263,7 @@ estrat_temp_invierno <- estrat_koppen %>%
 prop.table(estrat_temp_invierno)*100
 # La probabilidad que la temperatura sea <20º en invierno es de 26,48%
 
-## 3.d ---------------------------------------------------------------------
+## 3.d - Probabilidad de que lluevan entre 5mm y 10mm, en un clima Koppen Subtropical----
 
 ### Con el dataset original
 
@@ -260,7 +274,7 @@ data_1_d %>%
   count(lluvia >= 5 & lluvia <= 10) %>%
   prop.table()*100
 
-# La posibilidad que llueva entre 5 a 10mm en un clima Koppen Subtropical es de
+# La probabilidad que llueva entre 5 a 10mm en un clima Koppen Subtropical es de
 # 5,37%
 
 ### Con el dataset nuevo
@@ -272,7 +286,7 @@ estrat_subtropical %>%
   count(Lluvia >= 5 & Lluvia <= 10) %>%
   prop.table()*100
 
-# La posibilidad que llueva entre 5 a 10mm en un clima Koppen Subtropical es de
+# La probabilidad que llueva entre 5 a 10mm en un clima Koppen Subtropical es de
 # 5,10%
 
 ## Conclusión ----
@@ -282,15 +296,18 @@ estrat_subtropical %>%
 # mejores o peores
 
 
-# Pregunta 4 --------------------------------------------------------------
+# Pregunta 4 -------------------------------------------------------------------
 
-## 4.a----
+
+## 4.a - Para el promedio de temperatura de Australia a las 9am ----------------
 
 t.test(estrat_koppen$Temp9am, conf.level = 0.95)$conf.int[1:2]
 # Con un 95% de confianza, la temperatura promedio de Australia a las 9:00 AM
 # se encuentra en un rango entre 17,92 y 18,28
 
-## 4.b----
+
+## 4.b - Promedio de temperatura a las 9am en las distintas estaciones del año---- 
+## Compare sus intervalos
 
 estrat_koppen_verano <- estrat_koppen %>% 
   filter(Estacion == "Verano")
@@ -320,23 +337,25 @@ t.test(estrat_koppen_primavera$Temp9am, conf.level = 0.95)$conf.int[1:2]
 # Primavera -> 18.52897 19.19293
 
 
-### Conclusión----
+### Conclusión------------------------------------------------------------------
+
 # La variación entre los rangos de cada estación ronda 0,6º C
 # La diferencia de temperatura promedio entre otoño y primavera es de app 0.5º C
 # La diferencia de temperatura promedio entre verano e invierno es de app 10º C
 
 
-## 4.c ---------------------------------------------------------------------
+## 4.c - Proporción de días con lluvias-----------------------------------------
 
 t.test(estrat_koppen$LluviaHoy, conf.level = 0.95)$conf.int[1:2]*100 
   
-# Con un 95% de confianza, la posibilidad de días con lluvias 
+# Con un 95% de confianza, la probabilidad de días con lluvias 
 # oscila entre 21.20471% y 23.51529%
 
 
-## 4.d ---------------------------------------------------------------------
+## 4.d - Repetir con data original----------------------------------------------
 
-### a. Promedio temperatura----
+
+### a - Promedio temperatura 9am------------------------------------------------
 
 #### Con dataset original
 
@@ -349,7 +368,8 @@ t.test(estrat_koppen$Temp9am, conf.level = 0.95)$conf.int[1:2]# -> 17,92 y 18,28
 ## Los resultados se encuentran dentro del intervalo de confianza propuesto
 #  por el dataset estratificado
 
-### b. Promedio temperatura distintas estaciones----
+
+### b - Promedio temperatura a las 9 am en distintas estaciones-----------------
 
 # Como ya se hizo el ejercicio arriba, solamente se pegarán los valores
 # del dataset estratificado para ahorrar líneas de código
@@ -364,7 +384,6 @@ data_otoño <- data %>%
 t.test(data_otoño$Temp9am, conf.level = 0.95)$conf.int[1:2]
 # Otoño -> 18.02107 18.64323 (estratificado) -> 18.14532 18.36334 (original)
 
-
 data_invierno <- data %>% 
   filter(Estacion == "Invierno")
 t.test(data_invierno$Temp9am, conf.level = 0.95)$conf.int[1:2]
@@ -375,12 +394,14 @@ data_primavera <- data %>%
 t.test(data_primavera$Temp9am, conf.level = 0.95)$conf.int[1:2]
 # Primavera -> 18.52897 19.19293 (estratificado) -> 18.64564 18.88164 (original)
 
-#### Conclusión----
+
+#### Conclusión-----------------------------------------------------------------
+
 # Todos los valores originales se encuentran dentro del rango
 # propuesto por el intervalo estratificado
 
 
-### c. Días con lluvias ----
+### c. Días con lluvias --------------------------------------------------------
 
 # Dataset estratificado -> 21.20471% y 23.51529%
 t.test(estrat_koppen$LluviaHoy, conf.level = 0.95)$conf.int[1:2]*100
@@ -393,10 +414,10 @@ t.test(data$LluviaHoy, conf.level = 0.95)$conf.int[1:2]*100
 # propuesto por el intervalo estratificado
 
 
-# Pregunta 5  ---------------------------------------------------------------
+# Pregunta 5  ------------------------------------------------------------------
 
 
-## a. ----
+## 5.a - La temperatura promedio a a las 9am en Australia es menor a los 18°C----
 
 # H_0: Temp9am > 18
 # H_1: Temp9am < 18
@@ -407,7 +428,7 @@ t.test(estrat_koppen$Temp9am, mu=18, alternative = "g", conf.level = 0.9)
 # dado que el p-value es 0.12 con una significancia del 10%
 
 
-## b. ----
+## 5.b - Las horas de sol en una zona Koppen desértica es de 9 horas -----------
 
 # H_0: Horas de sol en el desierto = 9
 # H_1: Horas de sol en el desierto ≠ 9
@@ -421,7 +442,7 @@ t.test(koppen_desierto$Sol, mu=9, alternative = "two.sided", conf.level = 0.9)
 # dado que el p-value es 0.15 con una significancia del 10%
 
 
-## c. ----
+## 5.c - El porcentaje de humedad a las 3pm en invierno en una zona tropical es menor al 45%----
 
 # H_0: % humedad en una zona tropical < al 45%
 # H_1: % humedad en una zona tropical > al 45%
@@ -435,7 +456,7 @@ t.test(koppen_tropical_inv$Hum3pm, mu=45, alternative = "l", conf.level = 0.9)
 # dado que el p-value es 0.557 con una significancia del 10%
 
 
-## d. ----
+## 5.d - En invierno, la humedad a las 3pm es igual en una zona tropical y una zona subtropical----
 
 # Generar base estratificada subtropical
 
@@ -450,9 +471,9 @@ t.test(x = estrat_subtropical$Hum3pm, y = estrat_tropical$Hum3pm,
 # dado que el p-value es 0.00000000001873 con una significancia del 10%
 
 
-## e. ----
+## 5.e - Repetir con data original ---------------------------------------------
 
-### a. ----
+### a - La temperatura promedio a a las 9am en Australia es menor a los 18°C-----
 
 ## Dataset estratificado
 
@@ -470,7 +491,7 @@ t.test(data$Temp9am, mu=18, alternative = "g", conf.level = 0.9)
 # Con el original, no hay evidencia para rechazarla
 
 
-### b. ----
+### b - Las horas de sol en una zona Koppen desértica es de 9 horas ------------
 
 ## Dataset estratificado
 
@@ -493,7 +514,7 @@ t.test(data_desierto$Sol, mu=9, alternative = "two.sided", conf.level = 0.9)
 #  hipótesis 0
 # Con los datos completos la media de x baja bastante
 
-### c. ----
+### c - El porcentaje de humedad a las 3pm en invierno en una zona tropical es menor al 45%----
 
 ## Dataset estratificado
 
@@ -518,7 +539,7 @@ t.test(data_tropical_inv$Hum3pm, mu=45, alternative = "l", conf.level = 0.9)
 # Con los datos completos el p-value sube bastante y la media aumenta por casi
 # 1º Celsius
 
-### d. ----
+### d - En invierno, la humedad a las 3pm es igual en una zona tropical y una zona subtropical----
 
 ## Dataset Original
 
@@ -546,7 +567,7 @@ t.test(x = estrat_subtropical$Hum3pm, y = estrat_tropical$Hum3pm,
 
 # Pregunta 6 --------------------------------------------------------------
 
-## 6.a----
+## 6.a - Gráfico para el promedio de temperatura de Australia a las 9am ----------
 
 # Un primer approach que no se terminó ocupando
 # ggplot(data.frame(estrat_koppen), aes(x = Temp9am))+
@@ -560,7 +581,7 @@ table(estrat_koppen$Temp9am) %>%
           col = "darkgreen")
 
 
-## 6.b ---------------------------------------------------------------------
+## 6.b - Gráfico para promedio de temperatura a las 9am en las distintas estaciones del año----
 
 boxplot(estrat_koppen_verano$Temp9am, estrat_koppen_otono$Temp9am, 
         estrat_koppen_primavera$Temp9am, estrat_koppen_invierno$Temp9am, 
@@ -570,19 +591,21 @@ boxplot(estrat_koppen_verano$Temp9am, estrat_koppen_otono$Temp9am,
         names = c("Verano", "Otono", "Primavera", "Invierno"),
         col = "lightgreen")
 
-## 6.c ---------------------------------------------------------------------
+## 6.c - Gráfico para proporción de días con lluvias----------------------------
 
 barplot(prop.table(table(estrat_koppen$LluviaHoy)) *100,
-        main = "6.C: Posibilidad que llueva" ,
+        main = "6.C: Probabilidad que llueva" ,
         xlab = "Ocurrencia", 
         ylab = "%", 
-        names= c("No llueve: 22,36%", "Sí llueve, 77,64%"),
+        names= c("No llueve: 77,64%", "Sí llueve, 22,36%"),
         col = "darkblue")
 
+# El eje y (rango porcentajes) debería llegar hasta 78% sin embargo 
+# llega hasta 60, no pude detectar la razón de esto
 
-## 6.d ---------------------------------------------------------------------
+## 6.d - Repetir con la data original-------------------------------------------
 
-### a----
+### a - Gráfico para el promedio de temperatura de Australia a las 9am ---------
 
 table(data$Temp9am) %>%
   boxplot(notch = TRUE,
@@ -592,7 +615,7 @@ table(data$Temp9am) %>%
           col = "lightblue")
 # No pude detectar por qué el rango de temperatura está multiplicado por 10
 
-### b----
+### b - Gráfico para promedio de temperatura a las 9am en las distintas estaciones del año----
 
 boxplot(data_verano$Temp9am, data_otoño$Temp9am, 
         data_primavera$Temp9am, data_invierno$Temp9am, 
@@ -603,13 +626,14 @@ boxplot(data_verano$Temp9am, data_otoño$Temp9am,
         names = c("Verano", "Otono", "Primavera", "Invierno"),
         col = "lightgreen")
 
-### c----
+### c - Gráfico para proporción de días con lluvias-----------------------------
 
 barplot(prop.table(table(data$LluviaHoy)) *100,
-        main = "Posibilidad que llueva - Data Original" ,
+        main = "Probabilidad que llueva - Data Original" ,
         xlab = "Ocurrencia", 
         ylab = "%", 
         names= c("No llueve: 78,0375%", "Sí llueve,21,9625%"),
         col = "darkblue")
+
 # El eje y (rango porcentajes) debería llegar hasta 78% sin embargo 
 # llega hasta 60, no pude detectar la razón de esto
